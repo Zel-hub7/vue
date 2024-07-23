c0d082bffe55158a3790f47020357a8534a26926
unknown <zelalemyohannes75@gmail.com>
2024-07-23 14:17:01 +0300

    basic strucure of vue

---
+++ vue-project/src/components/ToDoItem.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <input type="checkbox" id="todo-item" />
+        <label for ="todo-Item">My Todo Item</label>
+
+    </div>
+
+</template>
+
+<script>
+    export default {};
+</script>
