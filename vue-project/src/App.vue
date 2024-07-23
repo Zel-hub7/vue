f9715e07b7e501eda726ae8edf9599641ea677b3
unknown <zelalemyohannes75@gmail.com>
2024-07-23 15:18:47 +0300

    commited

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,16 +1,43 @@
-<script setup>
-
-
+<script>
 import ToDoItem from './components/ToDoItem.vue';
+import  {nanoid} from "nanoid";
+export default {
+  name: "app",
+  components: {
+    ToDoItem,
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: "todo-" + nanoid(), label: "Learn Vue", done: false },
+        {
+          id: "todo-" + nanoid(),
+          label: "Create a Vue project with the CLI",
+          done: true,
+        },
+        { id: "todo-" + nanoid(), label: "Have fun", done: true },
+        {
+          id: "todo-" + nanoid(),
+          label: "Create a to-do list",
+          done: false,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
   <div id="app">
     <h1>To-Do List</h1>
-
+    <ul>
+  <li v-for="item in ToDoItems" :key="item.id">
+    <to-do-item :label="item.label" :done="item.done"></to-do-item>
+  </li>
+</ul>
   </div>
-  <ToDoItem></ToDoItem>
 </template>
 
 <style>
+/* Add any styles you need */
 </style>
--- vue-project/src/components/ToDoItem.vue
+++ vue-project/src/components/ToDoItem.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
         <input type="checkbox" id="todo-item" />
-        <label for ="todo-Item">My Todo Item</label>
+        <label for = "todo-item">{{ label }}</label>
 
     </div>
 
 </template>
 
 <script>
-    export default {};
+    export default {
+        props: {
+            label: {required: true, type: String },
+            done: {default: false, type: Boolean },
+        }
+    };
 </script>
